128c4e4a677583ca3fbb93ae111e4896f919824f
Mario <mariogv87@gmail.com>
2019-01-31 19:32:27 +0100

    add muliply function and refactor

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -4,8 +4,19 @@ Calculator.prototype = {
   _checkLengthArguments: (argumentsLength, spectedLength) => {
     if (argumentsLength < spectedLength) throw new Error('Incorrect number of arguments')
   },
+  _checkNegativeNumber: (numbers) => {
+    numbers.forEach(number => {
+      if (number < 0) throw new Error('Negative numbers are not allowed')
+    })
+  },
+  _getTotalNumbers: (a, args) => {
+    let totalNumbers = args.slice(0)
+    totalNumbers.push(a)
+    return totalNumbers
+  },
   substract: function (a, ...args) {
     this._checkLengthArguments(arguments.length, 2)
+    this._checkNegativeNumber(this._getTotalNumbers(a, args))
 
     return a - args.reduce((prev, next) => {
       return prev + next
@@ -13,11 +24,22 @@ Calculator.prototype = {
   },
   sum: function (a, ...args) {
     this._checkLengthArguments(arguments.length, 2)
+    this._checkNegativeNumber(this._getTotalNumbers(a, args))
 
     var b = args.reduce((prev, next) => {
       return prev + next
     })
     return a + b
+  },
+  multiply: function (a, ...args) {
+    this._checkLengthArguments(arguments.length, 2)
+    this._checkNegativeNumber(this._getTotalNumbers(a, args))
+    let result = a
+    args.forEach(element => {
+      result = result * element
+    })
+
+    return result
   }
 }
 
--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -14,7 +14,7 @@ describe('Calculator', () => {
       expect(oCalculator.substract(4, 2)).toEqual(2)
     })
 
-    it('Should substract should fail id it receives less than two numbers', () => {
+    it('Should substract should fail if it receives less than two numbers', () => {
       expect(() => {
         oCalculator.substract(4)
       }).toThrow()
@@ -24,8 +24,14 @@ describe('Calculator', () => {
       }).toThrow()
     })
 
-    it('Should substract 3 numbers (5 -1 - 2 = 2', () => {
+    it('Should substract 3 numbers', () => {
       expect(oCalculator.substract(5, 1, 2)).toEqual(2)
+    })
+
+    it('Should substract should fail if it receives an a negative number', () => {
+      expect(() => {
+        oCalculator.substract(-2, 3, -5, 1)
+      }).toThrow()
     })
   })
   describe('Sum', () => {
@@ -33,7 +39,7 @@ describe('Calculator', () => {
       expect(oCalculator.sum(4, 2)).toEqual(6)
     })
 
-    it('Should sum should fail id it receives less than two numbers', () => {
+    it('Should sum should fail if it receives less than two numbers', () => {
       expect(() => {
         oCalculator.substract(4)
       }).toThrow()
@@ -43,8 +49,39 @@ describe('Calculator', () => {
       }).toThrow()
     })
 
-    it('Should sum 3 numbers (5 + 1 + 2 = 8', () => {
+    it('Should sum 3 numbers', () => {
       expect(oCalculator.sum(5, 1, 2)).toEqual(8)
+    })
+
+    it('Should sum should fail if it receives an a negative number', () => {
+      expect(() => {
+        oCalculator.sum(-2, 3, -5, 1)
+      }).toThrow()
+    })
+  })
+  describe('Multiply', () => {
+    it('Should multiply at least two numbers', () => {
+      expect(oCalculator.multiply(4, 2)).toEqual(8)
+    })
+
+    it('Should multiply at least 4 numbers', () => {
+      expect(oCalculator.multiply(4, 2, 3, 5)).toEqual(120)
+    })
+
+    it('Should multiply should fail if it receives less than two numbers', () => {
+      expect(() => {
+        oCalculator.multiply(4)
+      }).toThrow()
+
+      expect(() => {
+        oCalculator.multiply()
+      }).toThrow()
+    })
+
+    it('Should multiply should fail if it receives an a negative number', () => {
+      expect(() => {
+        oCalculator.multiply(-2, 3, -5, 1)
+      }).toThrow()
     })
   })
 })
